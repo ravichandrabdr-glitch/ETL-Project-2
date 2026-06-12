6842f62e584bfe8c76a2374a7c94a0a319038c87
Ravichandra <ravichandrabdr@gmail.com>
2026-06-12 16:57:37 +0530

    My first commit

---
+++ Documents/PROJECTS/Retail_ETL_Project/retail_etl.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+
+
+project_root = Path(__file__).resolve().parent
+
+spark = SparkSession.builder \
+    .appName("Retail_ETL") \
+    .getOrCreate()
+
+customers = spark.read.csv(
+    str(project_root / "data" / "customers.csv"),
+    header=True,
+    inferSchema=True
+)
+
+orders = spark.read.csv(
+    str(project_root / "data" / "orders.csv"),
+    header=True,
+    inferSchema=True
+)
+customers.show()
+orders.show()
